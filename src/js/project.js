b8fcd9e6bfa7b4a61c2749538faca2faa8cd27ef
Miguel del Arco <migueldemollet10@gmail.com>
2024-12-26 17:16:36 +0100

    Added submenu for mobile devices and fix header margin

--- src/js/project.js
+++ src/js/project.js
@@ -1,6 +1,10 @@
 // Detectar secciones y actualizar el índice
 const sections = document.querySelectorAll('.content-father');
 const navLinks = document.querySelectorAll('.content-index a');
+const subheader = document.querySelector('.header-item.subheader');
+const subheaderText = document.querySelector('#current-section-text');
+
+subheader.style.display = 'none';
 
 window.addEventListener('scroll', () => {
     let currentSection = '';
@@ -14,10 +18,21 @@ window.addEventListener('scroll', () => {
         }
     });
 
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href').substring(1) === currentSection) {
-            link.classList.add('active');
+    if (currentSection) {
+        // Mostrar el subheader y actualizar el texto
+        //check if is a mobile device
+        if (window.innerWidth <= 700) {
+            subheader.style.display = 'flex';
         }
-    });
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').substring(1) === currentSection) {
+                link.classList.add('active');
+                subheaderText.textContent = link.textContent;
+            }
+        });
+    } else {
+        // Ocultar el subheader si no hay sección visible
+        subheader.style.display = 'none';
+    }
 });
