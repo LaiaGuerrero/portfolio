be23928916fcc22514d51b52f21395064c6340ca
LaiaGuerrero <laiaguerrero99@gmail.com>
2025-06-23 19:03:18 +0200

    Reviewed content for all project pages

--- src/js/expanded_cards.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('.overlay-text').forEach(item => {
-        item.addEventListener('click', function () {
-            const parent = item.parentElement;
-            parent.classList.toggle('expanded');
-        });
-    });
-});
